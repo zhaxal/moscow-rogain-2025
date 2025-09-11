1ffe0359f365dca867b3a97b14d7de461166b8fd
Anuarbek Zhakhangir <zhaxa65@gmail.com>
2025-09-12 07:12:52 +0900

    feat: update environment variables and remove unused dependencies; delete obsolete type definitions for questions and user answers

--- src/types/user.ts
+++
@@ -1,8 +0,0 @@
-import { RecordModel } from "pocketbase";
-
-export interface UserAnswer extends RecordModel {
-  question_id: string;
-  answer: string;
-  is_correct: boolean;
-  user_id: string;
-}
