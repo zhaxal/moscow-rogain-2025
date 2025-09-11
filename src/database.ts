0517f3b8ebb44f686371b1f76498d9f6f71ae0a5
Anuarbek Zhakhangir <zhaxa65@gmail.com>
2025-09-12 07:28:15 +0900

    feat: update database type definitions and import path for DB interface

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 import { Kysely, PostgresDialect } from "kysely";
-import { DB } from "kysely-codegen";
+import { DB } from "@/types/db";
 
 export const dialect = new PostgresDialect({
   pool: new Pool({
